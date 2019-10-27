3bee4400be693e5a5da2c3ef17dc6a95c7c05efe
jmayorga1 <jmayorga1@myancoraed.com>
2019-10-27 14:37:31 -0400

    First checkin

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BattingAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the place name");
+            string strName = Console.ReadLine();
+            Console.WriteLine("Enter the number of hits");
+            double nHits = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number of bats");
+            double nBats = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(strName + "'s batting average is: " +nHits/nBats);
+            Console.WriteLine("{0}'s batting average is {1}", strName, nHits / nBats);
+            
+        }
+    }
+}
